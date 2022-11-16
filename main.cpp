56aafaf00c4d9951caab4842e777aa29bbf51972
jeonghoon-lee <sherry0_@kakao.com>
2022-11-16 20:04:05 +0900

    change .c to .cpp

--- main.cpp
+++ main.cpp
@@ -24,25 +24,21 @@ char *choices[] = {
 };
 int n_choices = sizeof(choices) / sizeof(char *);
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     WINDOW *intro;
     WINDOW *window1;
     WINDOW *window2;
     WINDOW *window3;
 
     initscr();
-    noecho();        // 입력을 자동으로 화면에 출력하지 않도록 합니다.
+    noecho(); // 입력을 자동으로 화면에 출력하지 않도록 합니다.
     curs_set(FALSE); // cursor를 보이지 않게 합니다.
 
-    if (has_colors() == FALSE)
-    {
+    if (has_colors() == FALSE) {
         puts("Terminal does not support colors!");
         endwin();
         return 1;
-    }
-    else
-    {
+    } else {
         start_color();
         init_pair(GAMEW_PAIR_MAIN, COLOR_BLUE, COLOR_WHITE);
         init_pair(GAMEW_PAIR_R, COLOR_RED, COLOR_WHITE);
@@ -61,8 +57,7 @@ int main(int argc, char const *argv[])
     wbkgd(window2, COLOR_PAIR(SCOREW_PAIR));
     wbkgd(window3, COLOR_PAIR(MENUW_PAIR));
     int intro_result = draw_intro();
-    switch (intro_result)
-    {
+    switch (intro_result) {
     case 0:
         clear;
         draw_mainpage(window1, window2, window3);
@@ -83,8 +78,7 @@ int main(int argc, char const *argv[])
 }
 
 // 게임시작 - 화면 그리기
-void draw_mainpage(WINDOW *GAME, WINDOW *SIDE, WINDOW *FOOTER)
-{
+void draw_mainpage(WINDOW *GAME, WINDOW *SIDE, WINDOW *FOOTER) {
     draw_map(GAME);
     draw_side(SIDE);
     wborder(FOOTER, '|', '|', '-', '-', '+', '+', '+', '+');
@@ -94,34 +88,26 @@ void draw_mainpage(WINDOW *GAME, WINDOW *SIDE, WINDOW *FOOTER)
 }
 
 // 게임시작 - 오른쪽 그리기
-void draw_side(WINDOW *SIDE)
-{
+void draw_side(WINDOW *SIDE) {
     char *keyList[] = {
-        "  ^    :  Up",
-        "<- ->  :  Left/Right",
-        "  v    :  Down",
-        "",
-        "SPACE  :  Drop",
-        "K      :  Keep Blocks ",
-        "ESC    :  Exit",
-        "",
-        "Best   :  ",
-        "Score  :  ",
+        "  ^    :  Up",   "<- ->  :  Left/Right",
+        "  v    :  Down", "",
+        "SPACE  :  Drop", "K      :  Keep Blocks ",
+        "ESC    :  Exit", "",
+        "Best   :  ",     "Score  :  ",
         "LEVEL  :  ",
     };
     int n_keyList = sizeof(keyList) / sizeof(char *);
 
     wborder(SIDE, '|', '|', '-', '-', '+', '+', '+', '+');
-    for (int i = 0; i < n_keyList; i++)
-    {
+    for (int i = 0; i < n_keyList; i++) {
         mvwprintw(SIDE, 2 + i, 4, "%s", keyList[i]);
     }
     wrefresh(SIDE);
     draw_next_blocks(SIDE, n_keyList);
 }
 // 게임시작 - 오른쪽 subwin (next)
-void draw_next_blocks(WINDOW *SIDE, int start_point_y)
-{
+void draw_next_blocks(WINDOW *SIDE, int start_point_y) {
     WINDOW *next_blocks = subwin(SIDE, 10, 26, start_point_y + 3, 52);
     wbkgd(next_blocks, COLOR_PAIR(SCOREW_PAIR));
     // touchwin(SIDE);
@@ -130,23 +116,18 @@ void draw_next_blocks(WINDOW *SIDE, int start_point_y)
     wrefresh(next_blocks);
 }
 // 게임시작 - 게임판 그리기
-void draw_map(WINDOW *GAME)
-{
+void draw_map(WINDOW *GAME) {
     static int startpoint_x = 6;
     static int startpoint_y = 4;
 
     wborder(GAME, '|', '|', '-', '-', '+', '+', '+', '+');
     mvwprintw(GAME, 2, 19, "GAME BOARD");
-    for (int i = startpoint_y; i <= startpoint_y + 18; i += 2)
-    {
-        if ((i - startpoint_y) % 6 == 0)
-        {
+    for (int i = startpoint_y; i <= startpoint_y + 18; i += 2) {
+        if ((i - startpoint_y) % 6 == 0) {
             wattron(GAME, COLOR_PAIR(GAMEW_PAIR_MAIN));
             mvwprintw(GAME, i, startpoint_x,
                       "+---+---+---+---+---+---+---+---+---+");
-        }
-        else
-        {
+        } else {
             wattron(GAME, COLOR_PAIR(GAMEW_PAIR_R));
             mvwprintw(GAME, i, startpoint_x,
                       "+---+---+---+---+---+---+---+---+---+");
@@ -157,8 +138,7 @@ void draw_map(WINDOW *GAME)
             mvwprintw(GAME, i, startpoint_x + 36, "+");
         }
     }
-    for (int i = startpoint_y + 1; i <= startpoint_y + 18; i += 2)
-    {
+    for (int i = startpoint_y + 1; i <= startpoint_y + 18; i += 2) {
         wattron(GAME, COLOR_PAIR(GAMEW_PAIR_MAIN));
         mvwprintw(GAME, i, startpoint_x,
                   "|           |           |           |");
@@ -171,8 +151,7 @@ void draw_map(WINDOW *GAME)
 }
 
 // 초기화면 그리기
-int draw_intro()
-{
+int draw_intro() {
     static int window_size_x = 70;
     static int startpoint_x = 9;
     static int startpoint_y = 7;
@@ -186,10 +165,14 @@ int draw_intro()
     wbkgd(menu, COLOR_PAIR(SCOREW_PAIR));
 
     wborder(main, '|', '|', '-', '-', '+', '+', '+', '+');
-    mvwprintw(main, startpoint_y, startpoint_x, "#       #   ###   ###   ####   #   #  #  #  #   #");
-    mvwprintw(main, startpoint_y + 1, startpoint_x, "#   #   #  #   # #   #  #   #  #   #  ##    #   #");
-    mvwprintw(main, startpoint_y + 2, startpoint_x, " # # # #   #   # #   #  #   #  #   #  # #   #   #");
-    mvwprintw(main, startpoint_y + 3, startpoint_x, "  #   #     ###   ###   ###     ###   #  #   ###");
+    mvwprintw(main, startpoint_y, startpoint_x,
+              "#       #   ###   ###   ####   #   #  #  #  #   #");
+    mvwprintw(main, startpoint_y + 1, startpoint_x,
+              "#   #   #  #   # #   #  #   #  #   #  ##    #   #");
+    mvwprintw(main, startpoint_y + 2, startpoint_x,
+              " # # # #   #   # #   #  #   #  #   #  # #   #   #");
+    mvwprintw(main, startpoint_y + 3, startpoint_x,
+              "  #   #     ###   ###   ###     ###   #  #   ###");
     mvwprintw(main, 15, 50, "2019203032");
     mvwprintw(main, 16, 50, "2019203013");
     mvwprintw(main, 17, 50, "2017706057");
@@ -200,11 +183,9 @@ int draw_intro()
     wrefresh(main);
     draw_menu(menu, cursor);
 
-    while (1)
-    {
+    while (1) {
         c = wgetch(menu);
-        switch (c)
-        {
+        switch (c) {
         case KEY_LEFT:
             if (cursor == 0)
                 cursor = n_choices - 1;
@@ -231,24 +212,19 @@ int draw_intro()
     refresh();
 }
 // 초기화면 메뉴 그리기
-void draw_menu(WINDOW *menu, int highlight)
-{
+void draw_menu(WINDOW *menu, int highlight) {
     static int y = 5;
     static int x = 10;
     static int cursor = 0;
 
     wborder(menu, '|', '|', '-', '-', '+', '+', '+', '+');
     mvwprintw(menu, 2, 31, "menu");
-    for (int i = 0; i < n_choices; i++)
-    {
-        if (highlight == i)
-        {
+    for (int i = 0; i < n_choices; i++) {
+        if (highlight == i) {
             wattron(menu, A_REVERSE);
             mvwprintw(menu, y, x + (2 * x) * i, "%s", choices[i]);
             wattroff(menu, A_REVERSE);
-        }
-        else
-        {
+        } else {
             mvwprintw(menu, y, x + (2 * x) * i, "%s", choices[i]);
         }
     }
